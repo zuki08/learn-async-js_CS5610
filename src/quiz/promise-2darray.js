e55e33dcd5f747258de67b2eed3860a8a7f1aa0a
Joydeep Mitra <joy@Joydeeps-MacBook-Pro.local>
2024-02-23 14:10:30 -0500

    promise quiz

---
+++ src/quiz/promise-2darray.js
@@ -0,0 +1,35 @@
+function sum2DArray(arr) {
+    return new Promise((resolve, reject) => {
+        console.log('Sum called ... ');
+        if(Array.isArray(arr)) {
+            setTimeout(() => {
+                let sum = 0;
+                for (let i = 0; i < arr.length; i++) {
+                    for (let j = 0; j < arr[i].length; j++) {
+                        sum += arr[i][j];
+                    }
+                }
+                console.log('resolving ... ');
+                resolve(sum);
+            }, 0);
+        }
+        else {
+            console.log('rejecting ... ');
+            reject('BAD INPUT: Expected array as input');
+        }
+        console.log('returning from sum');
+    });
+}
+
+// Example usage:
+const array2D = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+const sumPromise1 = sum2DArray(array2D);
+console.log(sumPromise1);
+
+const sumPromise2 = sum2DArray('array2D');
+console.log(sumPromise2);
