d78b137d5c3f0bdc54defebe1f844b42842080db
Joydeep Mitra <joy@Joydeeps-MacBook-Pro.local>
2024-02-26 13:46:22 -0500

    concurrency promise

--- src/quiz/basic-server.js
+++ src/quiz/basic-server.js
@@ -37,7 +37,6 @@ const server = http.createServer((req, res) => {
     if (pathname.startsWith('/incidents/')) {
         // Extract parameters from the URL
         const [, , road, location, direction, id] = pathname.split('/');
-
         // Check if all parameters are provided
         if (road && location && direction && id) {
             // Send response with the extracted parameters
@@ -45,6 +44,10 @@ const server = http.createServer((req, res) => {
             res.end(JSON.stringify(incidents.filter((incident) => {
                 return incident.id === `MABOS00${id}`
             })));
+        }
+        else {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('Endpoint not found');
         }
     } else {
         // If the request is for an unsupported endpoint, send a not found response
